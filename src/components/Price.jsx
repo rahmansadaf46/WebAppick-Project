9b51abc6a16560ee0ba7aee6a71e580e2fe1cb8b
Sadaf <rahmansadaf46@gmail.com>
2024-08-26 14:19:54 +0600

    price section update

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,8 +1,22 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
 import check1 from "../assets/images/check-circle-1.png";
 import check2 from "../assets/images/check-circle-2.png";
+import { useState } from "react";
 
 const Price = () => {
+   const [hovered, setHovered] = useState([false, false, false]);
+
+   const handleMouseEnter = (index) => {
+     const newHoverState = [...hovered];
+     newHoverState[index] = true;
+     setHovered(newHoverState);
+   };
+
+   const handleMouseLeave = (index) => {
+     const newHoverState = [...hovered];
+     newHoverState[index] = false;
+     setHovered(newHoverState);
+   };
   return (
     <div>
       <div
@@ -25,183 +39,121 @@ const Price = () => {
         </p>
       </div>
       <Container>
-        <Row className="align-items-center my-5  justify-content-center">
-          <Col className="my-3" lg={4} md={12} xs={12}>
-            <Card>
-              <Card.Body>
-                <Card.Title>
-                  <h4>
-                    $50
-                    <span style={{ fontSize: "12px", color: "gray" }}>
-                      /1 month
-                    </span>
-                  </h4>
-                  <h4>Base</h4>
-                  <small style={{ fontSize: "12px", color: "gray" }}>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </small>
-                  <br />
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Access All Videos
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Get Certificate
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Chat support
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Update Notification
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Download material
-                  </small>
-                </Card.Title>
-
-                <div className="text-center">
-                  <button
-                    style={{
-                      background: "#FFFAF8",
-                      fontSize: "16px",
-                      borderRadius: "50px",
-                      color: "#FDC2A6",
-                    }}
-                    className="btn px-5 select-price-btn "
-                  >
-                    Choose Plan
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="my-3" lg={4} md={12} xs={12}>
-            <Card style={{ backgroundColor: "#245D51", color: "white" }}>
-              <Card.Body className="text-end">
-                <button
-                  style={{
-                    background: "#CB8461",
-                    fontSize: "10px",
-                    borderRadius: "50px",
-                    color: "white",
-                    width: "100px",
-                  }}
-                  className="btn"
-                >
-                  Most Popular
-                </button>
-              </Card.Body>
-              <Card.Body>
-                <Card.Title>
-                  <h4>
-                    $50
-                    <span style={{ fontSize: "12px", color: "white" }}>
-                      /1 month
-                    </span>
-                  </h4>
-                  <h4>Base</h4>
-                  <small style={{ fontSize: "12px", color: "white" }}>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </small>
-                  <br />
-                  <br />
-                  <small style={{ fontSize: "14px", color: "white" }}>
-                    <img src={check2} alt="" /> Access All Videos
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "white" }}>
-                    <img src={check2} alt="" /> Get Certificate
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "white" }}>
-                    <img src={check2} alt="" /> Chat support
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "white" }}>
-                    <img src={check2} alt="" /> Update Notification
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "white" }}>
-                    <img src={check2} alt="" /> Download material
-                  </small>
-                </Card.Title>
-
-                <div className="text-center">
-                  <button
-                    style={{
-                      background: "#CB8461",
-                      fontSize: "16px",
-                      borderRadius: "50px",
-                      color: "white",
-                    }}
-                    className="btn px-5 select-price-btn"
-                  >
-                    Choose Plan
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="my-3" lg={4} md={12} xs={12}>
-            <Card>
-              <Card.Body>
-                <Card.Title>
-                  <h4>
-                    $50
-                    <span style={{ fontSize: "12px", color: "gray" }}>
-                      /1 month
-                    </span>
-                  </h4>
-                  <h4>Base</h4>
-                  <small style={{ fontSize: "12px", color: "gray" }}>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </small>
-                  <br />
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Access All Videos
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Get Certificate
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Chat support
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Update Notification
-                  </small>
-                  <br />
-                  <small style={{ fontSize: "14px", color: "gray" }}>
-                    <img src={check1} alt="" /> Download material
-                  </small>
-                </Card.Title>
-
-                <div className="text-center">
-                  <button
-                    style={{
-                      background: "#FFFAF8",
-                      fontSize: "16px",
-                      borderRadius: "50px",
-                      color: "#FDC2A6",
-                    }}
-                    className="btn px-5 select-price-btn"
-                  >
-                    Choose Plan
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
+        <Row className="price-card-body d-flex align-items-center my-5 justify-content-center">
+          {[0, 1, 2].map((index) => (
+            <Col key={index} className="my-3" lg={3} md={12} xs={12}>
+              <Card
+                className="price-card"
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave(index)}
+                style={
+                  index === 1
+                    ? {
+                       
+                      }
+                    : { border: "none" }
+                }
+              >
+                {index === 1 && (
+                  <Card.Body className="text-end">
+                    <button
+                      style={{
+                        background: "#CB8461",
+                        fontSize: "10px",
+                        borderRadius: "50px",
+                        color: "white",
+                        width: "100px",
+                      }}
+                      className="btn"
+                    >
+                      Most Popular
+                    </button>
+                  </Card.Body>
+                )}
+                <Card.Body>
+                  <Card.Title>
+                    <h4>
+                      $50
+                      <span
+                        style={{
+                          fontSize: "12px",
+                        }}
+                      >
+                        /1 month
+                      </span>
+                    </h4>
+                    <h4>Base</h4>
+                    <small
+                      style={{
+                        fontSize: "12px",
+                      }}
+                    >
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </small>
+                    <br />
+                    <br />
+                    <small
+                      style={{
+                        fontSize: "14px",
+                      }}
+                    >
+                      <img src={hovered[index] ? check2 : check1} alt="" />{" "}
+                      Access All Videos
+                    </small>
+                    <br />
+                    <small
+                      style={{
+                        fontSize: "14px",
+                        
+                      }}
+                    >
+                      <img src={hovered[index] ? check2 : check1} alt="" /> Get
+                      Certificate
+                    </small>
+                    <br />
+                    <small
+                      style={{
+                        fontSize: "14px",
+                        
+                      }}
+                    >
+                      <img src={hovered[index] ? check2 : check1} alt="" /> Chat
+                      support
+                    </small>
+                    <br />
+                    <small
+                      style={{
+                        fontSize: "14px",
+                        
+                      }}
+                    >
+                      <img src={hovered[index] ? check2 : check1} alt="" />{" "}
+                      Update Notification
+                    </small>
+                    <br />
+                    <small
+                      style={{
+                        fontSize: "14px",
+                        
+                      }}
+                    >
+                      <img src={hovered[index] ? check2 : check1} alt="" />{" "}
+                      Download material
+                    </small>
+                  </Card.Title>
+                  <div className="text-center">
+                    <button
+                    
+                      className="btn px-5 select-price-btn"
+                    >
+                      Choose Plan
+                    </button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
         <br />
       </Container>
